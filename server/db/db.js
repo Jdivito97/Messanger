213ea9fc4a6cb1977b1a727582348193948f24cc
Jon DiVito <you@example.com>
2022-05-03 09:59:41 -0500

    add original default postgres URL from initial commit to server/db/db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,11 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const db = new Sequelize(process.env.REACT_APP_DATABASE_URL, {
-  logging: false,
-});
+const db = new Sequelize(
+  process.env.REACT_APP_DATABASE_URL || "postgres://localhost:5432/messenger",
+  {
+    logging: false,
+  }
+);
 
 module.exports = db;

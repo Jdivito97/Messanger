f20c0bdb382e1b2e57c9328efdc97325c1d89c1f
Jon DiVito <you@example.com>
2022-05-03 09:43:50 -0500

    Fix seed issues by adding dotenv module to server/app.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,7 @@
 const Sequelize = require("sequelize");
 console.log("env", process.env.REACT_APP_DATABASE_URL);
-
-// Hiding this URL in .env prevents the npm run seed command from working. npm run dev still functions perfectly if URL is hidden as .env variable
-const db = new Sequelize(
-  process.env.REACT_APP_DATABASE_URL ||
-    "postgres://postgres:Monkeytown@localhost:5432/messenger",
-  {
-    logging: false,
-  }
-);
+const db = new Sequelize(process.env.REACT_APP_DATABASE_URL, {
+  logging: false,
+});
 
 module.exports = db;
